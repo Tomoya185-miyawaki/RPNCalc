38080b94a1390c435e7b1a19da1cb5976a570b49
Tomoya <tomo16185@gmail.com>
2019-11-30 13:58:01 +0900

    [add]レイアウトの追加

--- App.js
+++ App.js
@@ -1,19 +1,86 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  StatusBar
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+const STATUSBAR_HEIGHT = Platform.OS == 'ios' ? 20 : StatusBar.currentHeight;
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.results}>
+          <View style={styles.resultLine}>
+          </View>
+          <View style={styles.resultLine}>
+          </View>
+          <View style={styles.resultLine}>
+          </View>
+        </View>
+        <View style={styles.buttons}>
+          <View style={styles.buttonsLine}>
+          </View>
+          <View style={styles.buttonsLine}>
+          </View>
+          <View style={styles.buttonsLine}>
+          </View>
+          <View style={styles.lastButtonLinesContainer}>
+            <View style={styles.twoButtonLines}>
+              <View style={styles.buttonsLine}>
+              </View>
+              <View style={styles.buttonsLine}>
+              </View>
+            </View>
+            <View style={styles.enterButtonContainer}>
+            </View>
+          </View>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: STATUSBAR_HEIGHT
   },
+  results: {
+    flex: 3,
+    backgroundColor: '#fff'
+  },
+  resultLine: {
+    flex: 1,
+    borderBottomWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'flex-end'
+  },
+  buttons: {
+    flex: 5
+  },
+  buttonsLine: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    borderWidth: 1
+  },
+  lastButtonLinesContainer: {
+    flex: 2,
+    flexDirection: 'row'
+  },
+  twoButtonLines: {
+    flex: 3
+  },
+  enterButtonContainer: {
+    flex: 1,
+    alignItems: 'center',
+    borderWidth: 1
+  }
 });
