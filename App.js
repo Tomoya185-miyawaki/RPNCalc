a7474d9448957f3800002ebe56e5d4321f224f8b
Tomoya <tomo16185@gmail.com>
2019-12-07 13:39:50 +0900

    [add]電卓のレイアウトの作成

--- App.js
+++ App.js
@@ -21,7 +21,112 @@ const CalcButton = (props) => {
   )
 }
 
+const CalcButtons = (props) => {
+  return (
+      <React.Fragment>
+        { props.buttons.map(button => {
+          return (
+            <CalcButton
+              key={button.label}
+              flex={button.flex}
+              label={button.label}
+              btnEvent={button.btnEvent} />
+          )
+        })}
+      </React.Fragment>
+  )
+}
+
 export default class App extends React.Component {
+
+  buttons = [
+    [
+      {
+        label: 'AC',
+        flex: 2,
+        btnEvent: () => {this.acButton()}
+      },
+      {
+        label: 'C',
+        btnEvent: () => {this.cButton()}
+      },
+      {
+        label: '+',
+        btnEvent: () => {this.calcButton('+')}
+      }
+    ],
+    [
+      {
+        label: '7',
+        btnEvent: () => {this.valueButton('7')},
+      },
+      {
+        label: '8',
+        btnEvent: () => {this.valueButton('8')},
+      },
+      {
+        label: '9',
+        btnEvent: () => {this.valueButton('9')},
+      },
+      {
+        label: '-',
+        btnEvent: () => {this.calcButton('-')},
+      },
+    ],
+    [
+      {
+        label: '4',
+        btnEvent: () => {this.valueButton('4')},
+      },
+      {
+        label: '5',
+        btnEvent: () => {this.valueButton('5')},
+      },
+      {
+        label: '6',
+        btnEvent: () => {this.valueButton('6')},
+      },
+      {
+        label: '*',
+        btnEvent: () => {this.calcButton('*')},
+      },
+    ],
+    [
+      {
+        label: '1',
+        btnEvent: () => {this.valueButton('1')},
+      },
+      {
+        label: '2',
+        btnEvent: () => {this.valueButton('2')},
+      },
+      {
+        label: '3',
+        btnEvent: () => {this.valueButton('3')},
+      },
+    ],
+    [
+      {
+        label: '0',
+        btnEvent: () => {this.valueButton('0')},
+      },
+      {
+        label: '.',
+        btnEvent: () => {this.valueButton('.')},
+      },
+      {
+        label: '/',
+        btnEvent: () => {this.calcButton('/')},
+      },
+    ],
+    [
+      {
+        label: 'Enter',
+        btnEvent: () => {this.enterButton()},
+      },
+    ]
+  ]
+
   valueButton = (value) => {
   }
 
@@ -50,22 +155,25 @@ export default class App extends React.Component {
         </View>
         <View style={styles.buttons}>
           <View style={styles.buttonsLine}>
-            <CalcButton flex={2} label={'AC'} btnEvent={() => this.acButton()} />
-            <CalcButton label={'C'} btnEvent={() => this.cButton()} />
-            <CalcButton label={'+'} btnEvent={() => this.calcButton()} />
+            <CalcButtons buttons={this.buttons[0]} />
           </View>
           <View style={styles.buttonsLine}>
+            <CalcButtons buttons={this.buttons[1]} />
           </View>
           <View style={styles.buttonsLine}>
+            <CalcButtons buttons={this.buttons[2]} />
           </View>
           <View style={styles.lastButtonLinesContainer}>
             <View style={styles.twoButtonLines}>
               <View style={styles.buttonsLine}>
+                <CalcButtons buttons={this.buttons[3]} />
               </View>
               <View style={styles.buttonsLine}>
+                <CalcButtons buttons={this.buttons[4]} />
               </View>
             </View>
             <View style={styles.enterButtonContainer}>
+              <CalcButtons buttons={this.buttons[5]} />
             </View>
           </View>
         </View>
